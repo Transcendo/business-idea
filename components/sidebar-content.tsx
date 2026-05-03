3c6023f3d1480cf495fd95a9967bd7f8ffbf8c85
djd <djd@djddeMac-mini.local>
2026-05-03 19:51:16 +0800

    content: add social scene the-breakfast

--- components/sidebar-content.tsx
+++ components/sidebar-content.tsx
@@ -115,6 +115,7 @@ export const contents: Content[] = [
 			{ title: "Luma", href: "/docs/social-relationship-products/luma", icon: () => <Activity className="w-4 h-4 text-current" /> },
 			{ title: "Partiful", href: "/docs/social-relationship-products/partiful", icon: () => <Activity className="w-4 h-4 text-current" /> },
 			{ title: "Timeleft", href: "/docs/social-relationship-products/timeleft", icon: () => <Activity className="w-4 h-4 text-current" /> },
+			{ title: "The Breakfast", href: "/docs/social-relationship-products/the-breakfast", icon: () => <Activity className="w-4 h-4 text-current" /> },
 			{ title: "222", href: "/docs/social-relationship-products/222", icon: () => <Activity className="w-4 h-4 text-current" /> },
 			{ title: "Mesh / Clay", href: "/docs/social-relationship-products/mesh-clay", icon: () => <Activity className="w-4 h-4 text-current" /> },
 			{ title: "Granola", href: "/docs/social-relationship-products/granola", icon: () => <Activity className="w-4 h-4 text-current" /> },
