265c90b0c32eca6838a950e083d97ba8ced8cb90
djd <djd@djddeMac-mini.local>
2026-05-04 15:34:34 +0800

    content: halfhour social relationship product update

--- components/sidebar-content.tsx
+++ components/sidebar-content.tsx
@@ -125,6 +125,7 @@ export const contents: Content[] = [
 			{ title: "Luma", href: "/docs/social-relationship-products/luma", icon: () => <Activity className="w-4 h-4 text-current" /> },
 			{ title: "Partiful", href: "/docs/social-relationship-products/partiful", icon: () => <Activity className="w-4 h-4 text-current" /> },
 			{ title: "Heylo", href: "/docs/social-relationship-products/heylo", icon: () => <Activity className="w-4 h-4 text-current" /> },
+			{ title: "Howbout", href: "/docs/social-relationship-products/howbout", icon: () => <Activity className="w-4 h-4 text-current" /> },
 			{ title: "Mozi", href: "/docs/social-relationship-products/mozi", icon: () => <Activity className="w-4 h-4 text-current" /> },
 			{ title: "Cappuccino", href: "/docs/social-relationship-products/cappuccino", icon: () => <Activity className="w-4 h-4 text-current" /> },
 			{ title: "Locket", href: "/docs/social-relationship-products/locket", icon: () => <Activity className="w-4 h-4 text-current" /> },
