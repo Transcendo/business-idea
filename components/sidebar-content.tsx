42798003df99fc81eb9bee764a7cc93faa65104a
djd <djd@djddeMac-mini.local>
2026-05-05 15:34:21 +0800

    content: halfhour social relationship product update

--- components/sidebar-content.tsx
+++ components/sidebar-content.tsx
@@ -226,6 +226,7 @@ export const contents: Content[] = [
 			{ title: "Supermomos", href: "/docs/social-relationship-products/supermomos", icon: () => <UsersRound className="w-4 h-4 text-current" /> },
 			{ title: "Beli", href: "/docs/social-relationship-products/beli", icon: () => <Utensils className="w-4 h-4 text-current" /> },
 			{ title: "Paired", href: "/docs/social-relationship-products/paired", icon: () => <HeartPulse className="w-4 h-4 text-current" /> },
+			{ title: "Cupla", href: "/docs/social-relationship-products/cupla", icon: () => <CalendarDays className="w-4 h-4 text-current" /> },
 			{ title: "Howbout", href: "/docs/social-relationship-products/howbout", icon: () => <Repeat2 className="w-4 h-4 text-current" /> },
 			{ title: "Mozi", href: "/docs/social-relationship-products/mozi", icon: () => <MapPinned className="w-4 h-4 text-current" /> },
 			{ title: "Cappuccino", href: "/docs/social-relationship-products/cappuccino", icon: () => <Coffee className="w-4 h-4 text-current" /> },
