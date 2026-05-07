20960dd6fcea424492c359194d077b5cd599d54a
djd <djd@djddeMac-mini.local>
2026-05-07 15:37:14 +0800

    docs(gaming): publish PEAK steam coop climbing case

--- components/sidebar-content.tsx
+++ components/sidebar-content.tsx
@@ -431,6 +431,7 @@ export const contents: Content[] = [
 			{ title: "Manor Lords", href: "/docs/gaming/manor-lords-steam-wishlist", icon: () => <Gamepad2 className="w-4 h-4 text-current" /> },
 			{ title: "Tiny Glade", href: "/docs/gaming/tiny-glade-steam-demo", icon: () => <Gamepad2 className="w-4 h-4 text-current" /> },
 			{ title: "R.E.P.O.", href: "/docs/gaming/repo-steam-coop-horror", icon: () => <Gamepad2 className="w-4 h-4 text-current" /> },
+			{ title: "PEAK", href: "/docs/gaming/peak-steam-coop-climbing", icon: () => <Gamepad2 className="w-4 h-4 text-current" /> },
 			{ title: "Lethal Company", href: "/docs/gaming/lethal-company-steam-coop-horror", icon: () => <Gamepad2 className="w-4 h-4 text-current" /> },
 			{ title: "Supermarket Together", href: "/docs/gaming/supermarket-together-steam-free-coop", icon: () => <Gamepad2 className="w-4 h-4 text-current" /> },
 			{ title: "Buckshot Roulette", href: "/docs/gaming/buckshot-roulette-steam-itch-viral", icon: () => <Gamepad2 className="w-4 h-4 text-current" /> },
